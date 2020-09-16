c1b57f9a867a1177b5b129d98b02c094e8da9d21
harshprajapai8347@gmail.com <harshprajapati8347@gmail.com>
2020-09-16 15:04:21 +0530

    Commited Craft

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import './Home.css'
 import Product from './Product'
+import PUgate from './Images/PUgate.png'
 
 function Home() {
     return (
         <div className="home">
             <div className="home__container">
-                <img className="home__image" src="https://m.media-amazon.com/images/S/abs-image-upload-na/a/AmazonStores/A21TJRUUN4KGV/9fcd9dd335fdf274ad0c798e955f0da8.w3000.h800._CR0%2C0%2C3000%2C800_SX1500_.png" alt=""/>
+                <img className="home__image" src={PUgate} alt=""/>
 
             <div className="home__row">
                 <Product />
-                {/* <Product /> */}
+                <Product />
+                <Product />
+                <Product />
             </div>
 
             <div className="home__row">
-                {/* <Product /> */}
-                {/* <Product /> */}
-                {/* <Product /> */}
+                <Product />
+                <Product />
+                <Product />
             </div>
 
             <div className="home__row">
-                {/* <Product /> */}
+                <Product />
             </div>
 
             </div>
